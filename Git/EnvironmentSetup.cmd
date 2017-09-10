38c6aa0796cc21d03790a2f620ba6d2d31e53b9c
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-09-10 16:54:27 +0100

    Added Git environment setup script

---
+++ Git/EnvironmentSetup.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+rem Username
+git congig --global user.name "Mark Ridgwell"
+git config --global user.email credfeto@users.noreply.github.com
+
+rem Git options
+git config --global pull.rebase true
