57dc175a4619425b362a5794772b58965a7426df
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-10-02 10:43:55 +0100

    Added code to determine if workspace is clean, and to pull rather than fetch

--- Git/fetch.cmd
+++ Git/fetch.cmd
@@ -1,4 +1,5 @@
 @echo off
+rem @echo on
 for /d %%a in (*.) do call :fetch %%a
 goto finish
 
@@ -12,10 +13,20 @@ echo.
 echo Retrieving %1:
 git remote get-url origin
 echo.
+rem Get latest from server
 git fetch
+
+rem determine whether to pull changes (i.e. if there is no changes in the workspace)
+git status > "%TEMP%\gitfetch.log"
+find /i "nothing to commit, working tree clean" "%TEMP%\gitfetch.log"
+IF %ERRORLEVEL% == 0 call :pull
 popd
 
 :notgit
+goto :eof
+
+:pull
+git pull
 goto :eof
 
 :finish
