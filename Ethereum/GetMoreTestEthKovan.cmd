8549efee1798b4f0f9e914adea725112b93e8741
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-10-12 17:51:13 +0100

    Added logging

--- Ethereum/GetMoreTestEthKovan.cmd
+++ Ethereum/GetMoreTestEthKovan.cmd
@@ -1,2 +1,5 @@
 @echo off
-curl http://github-faucet.kovan.network/url --data "address=https://gist.github.com/credfeto/11f8fda74bc86d5199b1bf0a5feb1384"
+IF "%WORK%" EQU "" SET WORK=%TEMP%
+SET FILENAME=%WORK%\Kovan.log
+echo %DATE% %TIME% >> %FILENAME%
+curl http://github-faucet.kovan.network/url --data "address=https://gist.github.com/credfeto/11f8fda74bc86d5199b1bf0a5feb1384" >> %FILENAME%
