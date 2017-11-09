ce0da4c302a4385e8b34fa4cde86e754bc10171b
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-11-09 11:56:13 +0000

    Added Explicit clean to make sure things pass

--- Ethereum/tr.cmd
+++ Ethereum/tr.cmd
@@ -4,6 +4,13 @@ pushd d:\work\Contracts-POC
 
 copy /y d:\work\truffle-localtest.js d:\work\Contracts-POC\truffle.js
 
+echo Cleaning...
+DEL /Q D:\Work\Contracts-POC\build\contracts
+
+echo Compiling...
+call truffle.cmd compile --network localtest --reset
+
+echo Testing...
 call truffle.cmd test --network localtest
 
 copy /y d:\work\truffle-original.js d:\work\Contracts-POC\truffle.js
