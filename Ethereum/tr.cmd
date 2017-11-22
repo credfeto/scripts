f5b1cf2ba02e57d9a3c0c7e55213d76db32979df
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-11-22 11:00:48 +0000

    Removed copies as no longer needed

--- Ethereum/tr.cmd
+++ Ethereum/tr.cmd
@@ -2,7 +2,7 @@
 d:
 pushd d:\work\Contracts-POC
 
-copy /y d:\work\truffle-localtest.js d:\work\Contracts-POC\truffle.js
+REM copy /y d:\work\truffle-localtest.js d:\work\Contracts-POC\truffle.js
 
 echo Cleaning...
 DEL /Q D:\Work\Contracts-POC\build\contracts
@@ -13,6 +13,6 @@ call truffle.cmd compile --network localtest --reset
 echo Testing...
 call truffle.cmd test --network localtest
 
-copy /y d:\work\truffle-original.js d:\work\Contracts-POC\truffle.js
+REM copy /y d:\work\truffle-original.js d:\work\Contracts-POC\truffle.js
 
 popd
