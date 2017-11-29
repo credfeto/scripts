3ae96c935a450dbb9ad6bcb12eabe837fc6bf0db
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-11-29 15:35:53 +0000

    Cleared screen before ruining

--- Ethereum/tr.cmd
+++ Ethereum/tr.cmd
@@ -1,11 +1,13 @@
 @echo off
-d:
-pushd d:\work\Contracts-POC
+cls
 
-REM copy /y d:\work\truffle-localtest.js d:\work\Contracts-POC\truffle.js
+d:
+pushd d:\work\Contracts
+
+REM copy /y d:\work\truffle-localtest.js d:\work\Contracts\truffle.js
 
 echo Cleaning...
-DEL /Q D:\Work\Contracts-POC\build\contracts
+DEL /Q d:\work\Contracts\build\contracts
 
 echo Compiling...
 call truffle.cmd compile --network localtest --reset
@@ -13,6 +15,6 @@ call truffle.cmd compile --network localtest --reset
 echo Testing...
 call truffle.cmd test --network localtest
 
-REM copy /y d:\work\truffle-original.js d:\work\Contracts-POC\truffle.js
+REM copy /y d:\work\truffle-original.js d:\work\Contracts\truffle.js
 
 popd
