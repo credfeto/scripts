7aeb05ecfbfa2a5c387ea5f1bfa09c77b1b0792b
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-11-30 13:57:22 +0000

    Removed commented out code

--- Ethereum/tr.cmd
+++ Ethereum/tr.cmd
@@ -4,8 +4,6 @@ cls
 d:
 pushd d:\work\Contracts
 
-REM copy /y d:\work\truffle-localtest.js d:\work\Contracts\truffle.js
-
 echo Cleaning...
 DEL /Q d:\work\Contracts\build\contracts
 
@@ -14,7 +12,5 @@ call truffle.cmd compile --network localtest --reset
 
 echo Testing...
 call truffle.cmd test --network localtest
-
-REM copy /y d:\work\truffle-original.js d:\work\Contracts\truffle.js
 
 popd
