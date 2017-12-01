c8544ae3e592b80954f72c99981b2ecf2e95c27e
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-12-01 14:27:28 +0000

    Added pruning

--- Git/EnvironmentSetup.cmd
+++ Git/EnvironmentSetup.cmd
@@ -1,8 +1,13 @@
 @echo off
 
 rem Username
-git congig --global user.name "Mark Ridgwell"
+git config --global user.name "Mark Ridgwell"
 git config --global user.email credfeto@users.noreply.github.com
 
 rem Git options
 git config --global pull.rebase true
+
+rem it Garbage Collection
+git config --global gc.auto=1
+git config --global gc.aggressivedepth=100
+git config --global gc.aggressivewindow=400
