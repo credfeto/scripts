0c69edf3e5f16f6766a2161e6ccf7dd8735ce84c
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-12-04 12:12:29 +0000

    Refactored solhint and compile so that they abort on error

--- Ethereum/tr.cmd
+++ Ethereum/tr.cmd
@@ -9,14 +9,40 @@ DEL /Q d:\work\Contracts\build\contracts
 
 echo Compiling...
 call truffle.cmd compile --network localtest --reset
+IF ERRORLEVEL 1 goto error
 
+SET LINTERRORS=0
 pushd Contracts
-for /r %%a in (*.sol) do call solhint %a%
+for /r %%a in (*.sol) do call :lint %%a
 popd
 
+IF %LINTERRORS% GTR 0 goto :error
+
+
+pause 
 echo Testing...
 call truffle.cmd test --network localtest
 
+goto finish
+
+:lint
+echo Linting %1
+call solhint %1
+IF ERRORLEVEL 1 SET /A LINTERRORS=%LINTERRORS%+1
+
+goto :eof
+
+:error
+ECHO *******************************************************************************
+ECHO **  ERRORS FOUND 
+ECHO *******************************************************************************
+
+
+:finish
+
+
 git checkout -- sharedABI/*.json
+
+
 
 popd
