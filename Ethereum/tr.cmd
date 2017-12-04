96a970d9366c23174d1c655ce30e39c3a868f4c7
Mark Ridgwell <credfeto@users.noreply.github.com>
2017-12-04 11:58:34 +0000

    Added Solhint to script

--- Ethereum/tr.cmd
+++ Ethereum/tr.cmd
@@ -10,6 +10,10 @@ DEL /Q d:\work\Contracts\build\contracts
 echo Compiling...
 call truffle.cmd compile --network localtest --reset
 
+pushd Contracts
+for /r %%a in (*.sol) do call solhint %a%
+popd
+
 echo Testing...
 call truffle.cmd test --network localtest
 
