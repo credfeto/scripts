35f1e1d806336058199dd7bf7e66d361a4883302
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-06-05 20:36:50 +0100

    Updated script

--- General/updatePackages.cmd
+++ General/updatePackages.cmd
@@ -1,6 +1,7 @@
 @echo off
 SET SPEC=FunFair.
 SET UPDATER=D:/Work/UpdatePackages/UpdatePackages/bin/Debug/netcoreapp2.0/UpdatePackages.dll
-for /f "usebackq tokens=1,2 delims= " %%a in (`nuget.exe list %SPEC%`) do dotnet %UPDATER% %%a %%b
 
-call resetnugetcache.cmd
+dotnet %UPDATER% -folder D:\Work -prefix %SPEC%
+
+rem call resetnugetcache.cmd
