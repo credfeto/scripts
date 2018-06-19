7d51887263ef98176c07c351969ce2505ce142bc
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-06-19 11:34:49 +0100

    Explicitly cleared the nuget http cache

--- General/updatePackages.cmd
+++ General/updatePackages.cmd
@@ -1,4 +1,7 @@
 @echo off
+rem clear the http cache so can see the latest set of packages
+nuget locals http-cache -clear
+
 SET SPEC=FunFair.
 SET UPDATER=D:/Work/UpdatePackages/UpdatePackages/bin/Debug/netcoreapp2.0/UpdatePackages.dll
 
