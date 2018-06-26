c753991bc7f2251387297242a5a7186006e795cb
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-06-26 12:38:40 +0100

    Changed to dotnet core 2.1

--- General/updatePackages.cmd
+++ General/updatePackages.cmd
@@ -3,7 +3,7 @@ rem clear the http cache so can see the latest set of packages
 nuget locals http-cache -clear
 
 SET SPEC=FunFair.
-SET UPDATER=D:/Work/UpdatePackages/UpdatePackages/bin/Debug/netcoreapp2.0/UpdatePackages.dll
+SET UPDATER=D:/Work/UpdatePackages/UpdatePackages/bin/Debug/netcoreapp2.1/UpdatePackages.dll
 
 dotnet %UPDATER% -folder D:\Work -prefix %SPEC%
 
