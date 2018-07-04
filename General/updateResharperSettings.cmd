8751b1f2daa56a3aa12672ded8d836186bde483b
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-07-04 20:54:27 +0100

    Updated scripts

--- General/updateResharperSettings.cmd
+++ General/updateResharperSettings.cmd
@@ -1,11 +1,48 @@
 @echo off
+SET SRCEDITORCONFIG=D:\Work\FunServer\.editorconfig
 SET SRC=D:\Work\FunServer\src\FunFair.FunServer.sln.DotSettings
+SET SRCCODEANALYSIS=D:\Work\FunServer\src\CodeAnalysis.ruleset
 
-for /R D:\Work %%a in (*.sln.DotSettings) do call :update %%a
+for /R D:\Work %%a in (*.sln.DotSettings) do call :updatedotsettings %%a
+for /d %%a in (D:\Work\*.*) do call :updateeditorconfig %%a
+for /d %%a in (D:\Work\*.*) do call :updatecodeanalysisrules %%a
 
 goto finish
 
-:update
+:updatecodeanalysisrules 
+echo %1
+IF NOT EXIST "%1\.git\HEAD" goto :noupdate
+IF "%1\src\CodeAnalysis.ruleset" == "%SRCCODEANALYSIS%" goto :noupdate
+
+pushd "%1"
+COPY /Y %SRCCODEANALYSIS%  %1\src\CodeAnalysis.ruleset
+
+git add src/CodeAnalysis.ruleset
+git commit -m"Updated %~NX1 CodeAnalysis.ruleset"
+git push
+
+popd
+
+goto :eof
+
+
+:updateeditorconfig 
+echo %1
+IF NOT EXIST "%1\.git\HEAD" goto :noupdate
+IF "%1\.editorconfig" == "%SRCEDITORCONFIG%" goto :noupdate
+
+pushd "%1"
+COPY /Y %SRCEDITORCONFIG%  %1\.editorconfig
+
+git add .editorconfig
+git commit -m"Updated %~NX1 .editorconfig"
+git push
+
+popd
+
+goto :eof
+
+:updatedotsettings
 IF "%SRC%" EQU "%1" goto noupdate
 IF NOT EXIST %1 goto noupdate
 
