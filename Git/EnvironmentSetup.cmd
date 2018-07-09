dfea478ea67e5845567a550c1e6081ac653c8718
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-07-09 16:04:29 +0100

    Updated scripts

--- Git/EnvironmentSetup.cmd
+++ Git/EnvironmentSetup.cmd
@@ -7,6 +7,9 @@ git config --global user.email credfeto@users.noreply.github.com
 rem Git options
 git config --global pull.rebase true
 
+rem Setup LG-> log
+git config --global alias.lg "log --oneline --color --decorate --graph --branches --tags"
+
 rem it Garbage Collection
 git config --global gc.auto=1
 git config --global gc.aggressivedepth=100
