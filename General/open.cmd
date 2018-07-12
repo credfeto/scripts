ce1dfb0345b6267b5e3806dcff900641060fd214
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-07-12 18:17:32 +0100

    Correcte paths

--- General/open.cmd
+++ General/open.cmd
@@ -2,8 +2,8 @@
 cd /d d:\work\
 call settitle.cmd
 
-start "FunServer" cd /d d:\work\cd FunServer
-start "Common" cd /d d:\work\cd Common
-start "Ethereum" cd /d d:\work\cd Ethereum
+start "FunServer" cd /d d:\work\FunServer
+start "Common" cd /d d:\work\Common
+start "Ethereum" cd /d d:\work\Ethereum
 start "ContentPackageManagement" cd /d d:\work\ContentPackageManagement
 start "ContentPackageBuilder" cd d:\work\ContentPackageBuilder
