1c910306bbc9521e744c41d1e83037d8c3abf40c
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-08-08 15:19:33 +0100

    Added comment

--- Ethereum/GetMoreTestEthRinkeby.cmd
+++ Ethereum/GetMoreTestEthRinkeby.cmd
@@ -9,4 +9,5 @@ REM echo.>> %FILENAME%
 REM echo ==================================================================================== >> %FILENAME%
 REM echo %DATE% %TIME% >> %FILENAME%
 REM curl http://github-faucet.kovan.network/url --data "address=https://gist.github.com/credfeto/11f8fda74bc86d5199b1bf0a5feb1384" >> %FILENAME%
-REM https://plus.google.com/+MarkRidgwell/posts/L3raHeqRPKa
+REM https://plus.google.com/u/1/b/107186216937940988217/+MarkridgwellCoUk/posts/71qG5awkCsY
+REM https://plus.google.com/u/1/b/107186216937940988217/+MarkridgwellCoUk/posts/gseqamonuWv
