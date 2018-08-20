01cc70e9f29d074eebb742b83827fbd5364947a9
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-08-20 14:00:37 +0100

    Added no-fast-forward to options

--- Git/EnvironmentSetup.cmd
+++ Git/EnvironmentSetup.cmd
@@ -6,6 +6,7 @@ git config --global user.email credfeto@users.noreply.github.com
 
 rem Git options
 git config --global pull.rebase true
+git config --global merge.ff false
 
 rem Setup LG-> log
 git config --global alias.lg "log --oneline --color --decorate --graph --branches --tags"
