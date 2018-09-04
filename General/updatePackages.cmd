92e66c14c32fd6b3345af1e5dae4c6ef1071d773
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-09-04 20:05:32 +0100

    Added packages

--- General/updatePackages.cmd
+++ General/updatePackages.cmd
@@ -6,8 +6,13 @@ SET SPEC=FunFair.
 SET UPDATER=D:/Work/UpdatePackages/UpdatePackages/bin/Debug/netcoreapp2.1/UpdatePackages.dll
 
 dotnet %UPDATER% -folder D:\Work -prefix %SPEC%
+
 SET SPEC=FluentValidation
 dotnet %UPDATER% -folder D:\Work -prefix %SPEC%
+
+SET SPEC=Microsoft
+dotnet %UPDATER% -folder D:\Work -prefix %SPEC%
+
 
 
 rem call resetnugetcache.cmd
