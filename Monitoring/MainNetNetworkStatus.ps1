5aa9e006c568ced7b1b73adfc4dd6149dd4cb0fe
Mark Ridgwell <credfeto@users.noreply.github.com>
2018-09-07 13:20:50 +0100

    Added Monitoring script for Mainnet/Showcase

---
+++ Monitoring/MainNetNetworkStatus.ps1
@@ -0,0 +1,25 @@
+﻿Try {
+
+    $status = Invoke-WebRequest -Uri https://showcase.funfair.io/api/networks/health?NetworkId=1  | Select-Object -ExpandProperty Content 
+    #$status = Invoke-WebRequest -Uri https://staging.funfair.io/api/networks/health?NetworkId=1  | Select-Object -ExpandProperty Content 
+    #$status = Invoke-WebRequest -Uri http://localhost:5000/api/networks/health?NetworkId=42  | Select-Object -ExpandProperty Content 
+
+    $status
+
+
+    if($status -eq "2") {
+        blink1-tool  --green
+    } elseif ($status -eq "1") {
+        blink1-tool --yellow --blink 5
+        blink1-tool --yellow
+    }
+    else
+    {
+        blink1-tool --red --blink 5
+        blink1-tool --red
+    }
+}
+Catch {
+    blink1-tool --red --blink 5
+    blink1-tool --red
+}
