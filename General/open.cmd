6ae2404cad14839b703b15ad3bda6da5e264aba8
Mark Ridgwell <credfeto@users.noreply.github.com>
2019-04-08 15:35:03 +0100

    Updated list of folders

--- General/open.cmd
+++ General/open.cmd
@@ -3,7 +3,9 @@ cd /d d:\work\
 call settitle.cmd
 
 start "FunServer" cd /d d:\work\FunServer
+start "FunWallet-Server" cd /d d:\work\FunWallet-Server
 start "Common" cd /d d:\work\Common
 start "Ethereum" cd /d d:\work\Ethereum
 start "ContentPackageManagement" cd /d d:\work\ContentPackageManagement
 start "ContentPackageBuilder" cd d:\work\ContentPackageBuilder
+start "Scripts" cd /d d:\work\Scripts
