29cb50652d8104a49c3a0b135e19b949cfb34a57
Mark Ridgwell <credfeto@users.noreply.github.com>
2019-07-09 11:41:43 +0100

    Updated monitoring script

--- Monitoring/MainNetNetworkStatus.ps1
+++ Monitoring/MainNetNetworkStatus.ps1
@@ -1,125 +1,36 @@
-﻿Try {
+﻿function CheckUrl($uri, $led) {
+    Try {
 
-    $status = Invoke-WebRequest -Uri https://casinofair.com/api/networks/health?NetworkId=1  | Select-Object -ExpandProperty Content 
+        $status = Invoke-WebRequest -Uri $uri | Select-Object -ExpandProperty Content 
 
-    $status
+        $status
 
-
-    if($status -eq "2") {
-        blink1-tool --id 0 --green
-    } elseif ($status -eq "1") {
-        blink1-tool --id 0 --rgb FF9900 --blink 5
-        blink1-tool --id 0 --rgb FF9900
+        if($status -eq "2") {
+            blink1-tool --id $led --green
+        } elseif ($status -eq "1") {
+            blink1-tool --id $led --rgb FF9900 --blink 5
+            blink1-tool --id $led --rgb FF9900
+        }
+        else
+        {
+            blink1-tool --id $led --red --blink 5
+            blink1-tool --id $led --red
+        }
     }
-    else
-    {
-        blink1-tool --id 0 --red --blink 5
-        blink1-tool --id 0 --red
-    }
-}
-Catch {
-    blink1-tool --id 0 --red --blink 5
-    blink1-tool --id 0 --red
-}
-
-
-Try {
-
-    $status = Invoke-WebRequest -Uri https://showcase.funfair.io/api/networks/health?NetworkId=4  | Select-Object -ExpandProperty Content 
-
-    $status
-
-
-    if($status -eq "2") {
-        blink1-tool --id 1 --green
-    } elseif ($status -eq "1") {
-        blink1-tool --id 1 --rgb FF9900 --blink 5
-        blink1-tool --id 1 --rgb FF9900
-    }
-    else
-    {
-        blink1-tool --id 1 --red --blink 5
-        blink1-tool --id 1 --red
+    Catch {
+        blink1-tool --id $led --red --blink 5
+        blink1-tool --id $led --red
     }
 }
-Catch {
-    blink1-tool --id 1 --red --blink 5
-    blink1-tool --id 1 --red
-}
 
 
+CheckUrl 'https://casinofair.com/api/networks/health?NetworkId=1' '1A0011F8'
 
-Try {
+CheckUrl 'https://showcase.funfair.io/api/networks/health?NetworkId=4' '1A001BBB'
 
-    $status = Invoke-WebRequest -Uri https://staging.funfair.io/api/networks/health?NetworkId=1  | Select-Object -ExpandProperty Content 
+CheckUrl 'https://staging.funfair.io/api/networks/health?NetworkId=1' '1A001C47'
 
-    $status
+CheckUrl 'https://dev.funfair.io/api/networks/health?NetworkId=1' '1A0020E0'
 
-
-    if($status -eq "2") {
-        blink1-tool --id 2  --green
-    } elseif ($status -eq "1") {
-        blink1-tool --id 2 --rgb FF9900 --blink 5
-        blink1-tool --id 2 --rgb FF9900
-    }
-    else
-    {
-        blink1-tool --id 2 --red --blink 5
-        blink1-tool --id 2 --red
-    }
-}
-Catch {
-    blink1-tool --id 2 --red --blink 5
-    blink1-tool --id 2 --red
-}
-
-Try {
-
-    $status = Invoke-WebRequest -Uri https://dev.funfair.io/api/networks/health?NetworkId=1  | Select-Object -ExpandProperty Content 
-
-    $status
-
-
-    if($status -eq "2") {
-        blink1-tool --id 3  --green
-    } elseif ($status -eq "1") {
-        blink1-tool --id 3 --rgb FF9900 --blink 5
-        blink1-tool --id 3 --rgb FF9900
-    }
-    else
-    {
-        blink1-tool --id 3 --red --blink 5
-        blink1-tool --id 3 --red
-    }
-}
-Catch {
-    blink1-tool --id 3 --red --blink 5
-    blink1-tool --id 3 --red
-}
-
-
-
-Try {
-
-    $status = Invoke-WebRequest -Uri https://localhost:5001/api/networks/health?NetworkId=1984  | Select-Object -ExpandProperty Content 
-
-    $status
-
-
-    if($status -eq "2") {
-        blink1-tool --id 4  --green
-    } elseif ($status -eq "1") {
-        blink1-tool --id 4 --rgb FF9900 --blink 5
-        blink1-tool --id 4 --rgb FF9900
-    }
-    else
-    {
-        blink1-tool --id 4 --red --blink 5
-        blink1-tool --id 4 --red
-    }
-}
-Catch {
-    blink1-tool --id 4 --red --blink 5
-    blink1-tool --id 4 --red
-}
+CheckUrl 'https://localhost:5001/api/networks/health?NetworkId=1984' '1A00230B'
 
