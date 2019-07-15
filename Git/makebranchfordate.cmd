4121d67d5c89c16b46e32bf6292919f4bb3bf636
Mark Ridgwell <credfeto@users.noreply.github.com>
2019-07-15 17:57:05 +0100

    Added script for creating a branch per day

---
+++ Git/makebranchfordate.cmd
@@ -0,0 +1,11 @@
+@echo off
+SET WHEN=
+FOR /F "tokens=1,2,3 delims=/" %%a in ("%DATE%") DO SET WHEN=%%c-%%d-%%a
+
+
+git fetch
+git checkout master
+git pull
+git checkout -b backup/%WHEN%
+git push --set-upstream origin backup/backup/%WHEN%
+git checkout master
