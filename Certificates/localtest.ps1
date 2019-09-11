dc67b7a943e6d2b66f3afe04e9712887bc662e5b
Mark Ridgwell <credfeto@users.noreply.github.com>
2019-09-11 16:41:08 +0100

    Powershell version of script

---
+++ Certificates/localtest.ps1
@@ -0,0 +1,4 @@
+﻿$when = Get-Date -Format "dddd MM/dd/yyyy HH:mm K"
+
+New-SelfSignedCertificate -DnsName *.ocaltest.me -CertStoreLocation cert:\LocalMachine\My -KeyLength 2048 -HashAlgorithm sha256  -KeyFriendlyName "LocalTest.me Wild Card {$when}"
+
