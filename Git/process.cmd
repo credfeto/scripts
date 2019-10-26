bdcd3433efc311bc5771a7959c6d7955782c025b
Mark Ridgwell <credfeto@users.noreply.github.com>
2019-10-26 19:24:52 +0100

    Process to update repos

--- Git/process.cmd
+++ Git/process.cmd
@@ -1,17 +1,46 @@
 @echo off
 
+
 rem git@github.com:funfair-tech/funfair-build-check.git
 
-SET ROOT=%~dp0
+SET ROOT=%CD%
 echo %ROOT%
 
-for /F %%a in (repos.lst) do call :checkrepo %%a
+for /F %%a in (%ROOT%\repos.lst) do call :checkrepo %%a
 
 goto :finish
 
+:updatepackage
+setlocal ENABLEDELAYEDEXPANSION
+set PACKAGE=%1
+
+ECHO * %PACKAGE%
+
+git reset head --hard
+git clean -f -x -d
+git checkout master
+git reset head --hard
+git clean -f -x -d
+
+rem %ROOT%\UpdatePackages\UpdatePackages\bin\Release\netcoreapp3.0\UpdatePackages.exe -folder %FOLDER% -prefix %PACKAGE%
+if exist src\*.sln cd src
+
+dotnet build --configuration Release
+IF ERRORLEVEL == 0 call :commit %PACKAGE%
+
+endlocal
+goto :EOF
+
+:commit
+echo *************************** UPDATE %1 ***************************
+REM git add *.csproj
+REM git commit -m"FF-1429 Updated Code %1 analysis package to latest version"
+
+goto :EOF
+
 
 :checkrepo
-SETLOCAL
+setlocal
 SET REPO=%1
 echo.
 echo Repo: %REPO%
@@ -20,17 +49,29 @@ for /F "tokens=1,2,3 delims=:/" %%b in ("%1") do set FOLDER=%%d
 SET FOLDER=%FOLDER:~0,-4%
 echo %FOLDER%
 
-IF NOT EXIST %ROOT%%FOLDER% git clone %REPO%
-PUSHD %ROOT%%FOLDER%
-
+IF NOT EXIST %ROOT%\%FOLDER%\.git\HEAD git clone %REPO%
+cd /d %ROOT%
+PUSHD %ROOT%\%FOLDER%
 ECHO %CD%
+
+git fetch
+git rebase
+
+call :updatepackage AsyncFixer
+call :updatepackage NSubstitute.Analyzers.CSharp
+call :updatepackage Microsoft.CodeAnalysis.FxCopAnalyzers
+call :updatepackage SonarAnalyzer.CSharp
+call :updatepackage xunit.analyzers
+call :updatepackage SourceLink.Create.CommandLine
 
 POPD
 
-rd /s /q %ROOT%%FOLDER%
+REM rd /s /q %ROOT%\%FOLDER%
 
-ENDLOCAL
-goto :eof
+endlocal
+goto :EOF
+
+
 
 
 :finish
