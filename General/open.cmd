74c6c58a360a83e665aba67582f6e4af87f9cfce
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-02-12 13:53:26 +0000

    Updated list of things to open

--- General/open.cmd
+++ General/open.cmd
@@ -9,3 +9,10 @@ start "Ethereum" cd /d d:\work\Ethereum
 start "ContentPackageManagement" cd /d d:\work\ContentPackageManagement
 start "ContentPackageBuilder" cd d:\work\ContentPackageBuilder
 start "Scripts" cd /d d:\work\Scripts
+start "Funfair.Random" cd /d d:\work\Funfair.Random
+start "Swagger" cd /d d:\work\Swagger
+start "Alerts" cd /d d:\work\Alerts
+start "funfair-server-code-analysis" cd /d d:\work\funfair-server-code-analysis
+start "funfair-server-template" cd /d d:\work\funfair-server-template
+start "funfair-server-test" cd /d d:\work\funfair-server-test
+start "Automation" cd /d d:\Automation
