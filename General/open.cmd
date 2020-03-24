9933a87dc7d4e53bb40b85b235bf09f453ee7fb6
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-03-24 09:22:44 +0000

    Changed to use renamed repo names

--- General/open.cmd
+++ General/open.cmd
@@ -2,17 +2,16 @@
 cd /d d:\work\
 call settitle.cmd
 
-start "FunServer" cd /d d:\work\FunServer
-start "FunWallet-Server" cd /d d:\work\FunWallet-Server
-start "Common" cd /d d:\work\Common
-start "Ethereum" cd /d d:\work\Ethereum
-start "ContentPackageManagement" cd /d d:\work\ContentPackageManagement
-start "ContentPackageBuilder" cd d:\work\ContentPackageBuilder
-start "Scripts" cd /d d:\work\Scripts
-start "Funfair.Random" cd /d d:\work\Funfair.Random
-start "Swagger" cd /d d:\work\Swagger
-start "Alerts" cd /d d:\work\Alerts
+start "funfair-casino-server" cd /d d:\work\funfair-casino-server
+start "funfair-wallet-server" cd /d d:\work\funfair-wallet-server
+start "funfair-server-common" cd /d d:\work\funfair-server-common
+start "funfair-server-ethereum" cd /d d:\work\funfair-server-ethereum
+start "funfair-server-content-package-management" cd /d d:\work\funfair-server-content-package-management
+start "funfair-server-content-package-builder" cd d:\work\funfair-server-content-package-builder
+start "scripts" cd /d d:\work\scripts
+start "funfair-server-random" cd /d d:\work\funfair-server-random
+start "funfair-server-swagger" cd /d d:\work\funfair-server-swagger
+start "funfair-server-alerts" cd /d d:\work\funfair-server-alerts
 start "funfair-server-code-analysis" cd /d d:\work\funfair-server-code-analysis
 start "funfair-server-template" cd /d d:\work\funfair-server-template
 start "funfair-server-test" cd /d d:\work\funfair-server-test
-start "Automation" cd /d d:\Automation
