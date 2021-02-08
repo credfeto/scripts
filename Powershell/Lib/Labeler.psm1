16c306f6d10b31957c7d2ec889393a600ae57af7
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-02-08 22:58:18 +0000

    Updated labeler script"

--- Powershell/Lib/Labeler.psm1
+++ Powershell/Lib/Labeler.psm1
@@ -42,18 +42,21 @@ param(
     $config = @(
         [pscustomobject]@{
             Name="C#"
+            Description="C# Source Files"
             Colour="db6baa"
             Paths = @( "src/**/*.cs",
                        "src/**/*.csproj" )
         },
         [pscustomobject]@{
             Name="Powershell"
+            Description="Powershell Source Files"
             Colour="23bc12"
             Paths = @( "**/*.ps1",
                        "**/*.psm1" )
         },
         [pscustomobject]@{
             Name="SQL"
+            Description="SQL Source Files"
             Colour="413cd1"
             Paths = @( "db/**/*",
                        "tools/**/*.sql" )
@@ -61,11 +64,13 @@ param(
         },
         [pscustomobject]@{
             Name="Solidity"
+            Description="Solidity Source Files"
             Colour="413cd1"
             Paths = @( "src/**/*.sol" )
         },
         [pscustomobject]@{
             Name="unit-tests"
+            Description="Unit test and integration test projects"
             Colour="0e8a16"
             Paths = @( "src/*.Tests.*/**/*",
                        "src/*.Tests.Integration.*/**/*",
@@ -74,106 +79,121 @@ param(
         },
         [pscustomobject]@{
             Name=".NET update"
+            Description="Update to .net global.json"
             Colour="a870c9"
             Paths = @( "src/global.json" )
         },
         [pscustomobject]@{
             Name="Config Change"
+            Description="Configuration files changes"
             Colour="d8bb50"
             Paths = @( "src/**/*.json" )
         },
         [pscustomobject]@{
             Name="Static Code Analysis Rules"
+            Description="Ruleset for static code analysis files"
             Colour="00dead"
             Paths = @( "src/CodeAnalysis.ruleset" )
         },
         [pscustomobject]@{
             Name="Migration Script"
+            Description="SQL Migration scripts"
             Colour="b680e5"
             Paths = @( "tools/MigrationScripts/**/*" )
         },
         [pscustomobject]@{
             Name="Legal Text"
+            Description="Legal text files"
             Colour="facef0"
             Paths = @( "tools/LegalText/**/*" )
         },
         [pscustomobject]@{
             Name="Change Log"
+            Description="Changelog tracking file"
             Colour="53fcd4"
             Paths = @( "CHANGELOG.md" )
         },
         [pscustomobject]@{
             Name="Read Me"
+            Description="Repository readme file"
             Colour="5319e7"
             Paths = @( "README.md" )
         },
         [pscustomobject]@{
             Name="Setup"
+            Description="Setup instructions"
             Colour="5319e7"
             Paths = @( "SETUP.md" )
         },
         [pscustomobject]@{
             Name="github-actions"
+            Description="Github actions workflow files"
             Colour="e09cf4"
             Paths = @( ".github/workflows/*.yml" )
         },
         [pscustomobject]@{
             Name="Tech Debt"
+            Description="Technical debt"
             Colour="30027a"
             Paths = @()
         },
         [pscustomobject]@{
             Name="auto-pr"
+            Description="Pull request created automatically"
             Colour="0000aa"
             Paths = @()
         },
         [pscustomobject]@{
-            Name=".NET Update"
-            Colour="a870c9"
-            Paths = @()
-        },
-        [pscustomobject]@{
             Name="no-pr-activity"
+            Description="Pull Request has had no activity for a long time"
             Colour="ffff00"
             Paths = @()
         },
         [pscustomobject]@{
             Name="!!! WAITING FOR CLIENT PR"
+            Description="Pull request needs a client pull request to be merged at the same time"
             Colour="ffff00"
             Paths = @()
         },
         [pscustomobject]@{
             Name="!!! WAITING FOR WALLET PR"
+            Description="Pull request needs a wallet pull request to be merged at the same time"
             Colour="ffff00"
             Paths = @()
         },
         [pscustomobject]@{
             Name="!!! WAITING FOR SERVER PR"
+            Description="Pull request needs a server pull request to be merged at the same time"
             Colour="ffff00"
             Paths = @()
         },
         [pscustomobject]@{
             Name="!!! WAITING FOR QA SIGNOFF"
+            Description="Pull request needs a QA Signoff before it can be merged"
             Colour="ffff00"
             Paths = @()
         },
         [pscustomobject]@{
             Name="dependencies"
+            Description="Updates to dependencies"
             Colour="0366d6"
             Paths = @()
         },
         [pscustomobject]@{
             Name="dotnet"
+            Description="Dotnet package updates"
             Colour="db6baa"
             Paths = @()
         },
         [pscustomobject]@{
             Name="npm"
+            Description="npm package upate"
             Colour="e99695"
             Paths = @()
         },
         [pscustomobject]@{
             Name="DO NOT MERGE"
+            Description="This pull request should not be merged yey"
             Colour="ff0000"
             Paths = @()
         }
@@ -198,6 +218,7 @@ param(
 
                 $newLabel = [pscustomobject]@{
                                 Name=$labelName
+                                Description="Changes in $name project"
                                 Colour=$colour
                                 Paths = @("src/$name/**/*")
                             }
@@ -225,7 +246,7 @@ param(
             $labeller += ''
         }
 
-        $labelsWithColour += ' - name: "' + $group.Name
+        $labelsWithColour += ' - name: "' + $group.Name + '"'
         $labelsWithColour += '   color: "' + $group.Colour + '"'
         $labelsWithColour += ''
 
