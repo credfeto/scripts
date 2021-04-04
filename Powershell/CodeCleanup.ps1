4367295643da04c29f0234f8659a7ff8f92b5658
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-04 23:05:26 +0100

    Code cleanup of the solution

--- Powershell/CodeCleanup.ps1
+++ Powershell/CodeCleanup.ps1
@@ -57,7 +57,101 @@ catch {
 }
 #endregion
 
+function fixResharperSuppression($sourceFolder) {
+    Write-Information "* Changing Resharper disable once comments to SuppressMessage"
+    Write-Information "  - Folder: $sourceFolder"
 
+    $emptyLine = [char]13 + [char]10
+
+    $linesToRemoveRegex = "(?<LinesToRemove>((\r\n){2,}))"
+    $suppressMessageRegex = "(?<End>\s+\[(System\.Diagnostics\.CodeAnalysis\.)?SuppressMessage)"
+    $removeBlankLinesRegex = "(?ms)" +  "(?<Start>(^((\s+)///\s+</(.*?)\>)))" + $linesToRemoveRegex + $suppressMessageRegex
+    $removeBlankLines2Regex = "(?ms)" + "(?<Start>(^((\s+)///\s+<(.*?)/\>)))" + $linesToRemoveRegex + $suppressMessageRegex
+
+    $replacements = "RedundantDefaultMemberInitializer",
+                    "ParameterOnlyUsedForPreconditionCheck.Global",
+                    "ParameterOnlyUsedForPreconditionCheck.Local",
+                    "UnusedMember.Global",
+                    "UnusedMember.Local",
+                    "AutoPropertyCanBeMadeGetOnly.Global",
+                    "AutoPropertyCanBeMadeGetOnly.Local",
+                    "ClassNeverInstantiated.Local",
+                    "ClassNeverInstantiated.Global",
+                    "ClassCanBeSealed.Global",
+                    "ClassCanBeSealed.Local",
+                    "UnusedAutoPropertyAccessor.Global",
+                    "UnusedAutoPropertyAccessor.Local",
+                    "MemberCanBePrivate.Global",
+                    "MemberCanBePrivate.Local",
+                    "InconsistentNaming",
+                    "IdentifierTypo",
+                    "UnusedTypeParameter",
+                    "HeapView.BoxingAllocation"
+                    "UnusedType.Local",
+                    "UnusedType.Global",
+                    "PrivateFieldCanBeConvertedToLocalVariable"
+
+    $files = Get-ChildItem -Path $sourceFolder -Filter "*.cs" -Recurse
+    ForEach($file in $files) {
+        $fileName = $file.FullName
+
+        $content = Get-Content -Path $fileName -Raw
+        $originalContent = $content
+        $updatedContent = $content
+
+        $changedFile = $False
+
+        ForEach($replacement in $replacements) {
+            $code = $replacement.Replace(".", "\.")
+            $regex = "//\s+ReSharper\s+disable\s+once\s+$code"
+            $replacementText = "[System.Diagnostics.CodeAnalysis.SuppressMessage(""ReSharper"", ""$replacement"", Justification=""TODO: Review"")]"
+
+            $updatedContent = $content -replace $regex, $replacementText
+            if($content -ne $updatedContent)
+            {
+                $content = $updatedContent
+                if($changedFile -eq $False) {
+                    Write-Information "* $fileName"
+                    $changedFile = $True
+                }
+
+                Write-Information "   - Changed $replacement comment to SuppressMessage"
+            }
+        }
+
+
+        $replacementText = '${Start}' + $emptyLine + '${End}'
+        $updatedContent = $content -replace $removeBlankLinesRegex, $replacementText
+        if($content -ne $updatedContent)
+        {
+            $content = $updatedContent
+            if($changedFile -eq $False) {
+                Write-Information "* $fileName"
+                $changedFile = $True
+            }
+
+            Write-Information "   - Removed blank lines (end tag)"
+        }
+
+
+        $replacementText = '${Start}' + $emptyLine + '${End}'
+        $updatedContent = $content -replace $removeBlankLines2Regex, $replacementText
+        if($content -ne $updatedContent)
+        {
+            $content = $updatedContent
+            if($changedFile -eq $False) {
+                Write-Information "* $fileName"
+                $changedFile = $True
+            }
+
+            Write-Information "   - Removed blank lines (single tag)"
+        }
+
+        if($content -ne $originalContent) {
+            Set-Content -Path $fileName -Value $content
+        }
+    }
+}
 
 function runCodeCleanup($solutionFile) {
 
@@ -75,105 +169,14 @@ function runCodeCleanup($solutionFile) {
     $buildOk = DotNet-BuildSolution -srcFolder $sourceFolder
     if($buildOk -eq $true) {
 
-        Write-Information "* Changing Resharper disable once comments to SuppressMessage"
-        Write-Information "  - Folder: $sourceFolder"
-
-        $emptyLine = [char]13 + [char]10
-
-        $linesToRemoveRegex = "(?<LinesToRemove>((\r\n){2,}))"
-        $suppressMessageRegex = "(?<End>\s+\[(System\.Diagnostics\.CodeAnalysis\.)?SuppressMessage)"
-        $removeBlankLinesRegex = "(?ms)" +  "(?<Start>(^((\s+)///\s+</(.*?)\>)))" + $linesToRemoveRegex + $suppressMessageRegex
-        $removeBlankLines2Regex = "(?ms)" + "(?<Start>(^((\s+)///\s+<(.*?)/\>)))" + $linesToRemoveRegex + $suppressMessageRegex
-
-        $replacements = "RedundantDefaultMemberInitializer",
-                        "ParameterOnlyUsedForPreconditionCheck.Global",
-                        "ParameterOnlyUsedForPreconditionCheck.Local",
-                        "UnusedMember.Global",
-                        "UnusedMember.Local",        
-                        "AutoPropertyCanBeMadeGetOnly.Global",
-                        "AutoPropertyCanBeMadeGetOnly.Local",
-                        "ClassNeverInstantiated.Local",
-                        "ClassNeverInstantiated.Global",
-                        "ClassCanBeSealed.Global",
-                        "ClassCanBeSealed.Local",
-                        "UnusedAutoPropertyAccessor.Global",
-                        "UnusedAutoPropertyAccessor.Local",
-                        "MemberCanBePrivate.Global",
-                        "MemberCanBePrivate.Local",
-                        "InconsistentNaming",
-                        "IdentifierTypo",
-                        "UnusedTypeParameter",
-                        "HeapView.BoxingAllocation"
-                        "UnusedType.Local",
-                        "UnusedType.Global",
-                        "PrivateFieldCanBeConvertedToLocalVariable"
-
-        $files = Get-ChildItem -Path $srcPath -Filter "*.cs" -Recurse
-        ForEach($file in $files) {
-            $fileName = $file.FullName
-
-            $content = Get-Content -Path $fileName -Raw
-            $originalContent = $content
-            $updatedContent = $content
-
-            $changedFile = $False
-
-            ForEach($replacement in $replacements) {
-                $code = $replacement.Replace(".", "\.")
-                $regex = "//\s+ReSharper\s+disable\s+once\s+$code"
-                $replacementText = "[System.Diagnostics.CodeAnalysis.SuppressMessage(""ReSharper"", ""$replacement"", Justification=""TODO: Review"")]"
-
-                $updatedContent = $content -replace $regex, $replacementText
-                if($content -ne $updatedContent)
-                {
-                    $content = $updatedContent
-                    if($changedFile -eq $False) {
-                        Write-Information "* $fileName"
-                        $changedFile = $True
-                    }
-
-                    Write-Information "   - Changed $replacement comment to SuppressMessage"                    
-                }
-            }
-
-
-            $replacementText = '${Start}' + $emptyLine + '${End}'
-            $updatedContent = $content -replace $removeBlankLinesRegex, $replacementText
-            if($content -ne $updatedContent)
-            {
-                $content = $updatedContent
-                if($changedFile -eq $False) {
-                    Write-Information "* $fileName"
-                    $changedFile = $True
-                }
-
-                Write-Information "   - Removed blank lines (end tag)"
-            }
-
-
-            $replacementText = '${Start}' + $emptyLine + '${End}'
-            $updatedContent = $content -replace $removeBlankLines2Regex, $replacementText
-            if($content -ne $updatedContent)
-            {
-                $content = $updatedContent
-                if($changedFile -eq $False) {
-                    Write-Information "* $fileName"
-                    $changedFile = $True
-                }
-
-                Write-Information "   - Removed blank lines (single tag)"
-            }
-
-            if($content -ne $originalContent) {
-                Set-Content -Path $fileName -Value $content
-            }
-        }
+        fixResharperSuppression -sourceFolder $sourceFolder
 
         Write-Information "* Running Code Cleanup"
         Write-Information "  - Solution: $Solution"
         Write-Information "  - Cache Folder: $cachesFolder"
         Write-Information "  - Settings File: $settingsFile"
 
+        # Cleanup each project
         $projects = Get-ChildItem -Path $sourceFolder -Filter "*.csproj" -Recurse
         ForEach($project in $projects) {
             $projectFile = $project.FullName
@@ -186,14 +189,21 @@ function runCodeCleanup($solutionFile) {
             }
         }
 
+        # Cleanup the solution
+        dotnet jb cleanupcode --profile="Full Cleanup" $solutionFile --properties:Configuration=Release --caches-home:"$cachesFolder" --settings:"$settingsFile" --verbosity:INFO --no-buildin-settings
+        if(!$?) {
+            Write-Information ">>>>> Code Cleanup failed"
+            return $false
+        }
+
         Write-Information "* Building after cleanup"
         $result = DotNet-BuildSolution -srcFolder $sourceFolder
         if($result -eq $true) {
             return $true
         }
 
-	Write-Information ">>>>> Build Failed!"
-	exit
+	    Write-Information ">>>>> Build Failed!"
+	    exit
     }
 
     Write-Information ">>>>> Build Failed!"
@@ -230,7 +240,6 @@ function processRepo($srcRepo, $repo) {
     if( $lastRevision -eq $currentRevision) {
         Write-Information "Repo not changed"
     }
-
 
     $hasCleanedSuccessFully = $false
     #########################################################
@@ -290,14 +299,12 @@ function processRepo($srcRepo, $repo) {
 
 #########################################################################
 
-
 $installed = DotNetTool-Install -packageId $packageIdToInstall -preReleaseVersion $preRelease
 
 if($installed -eq $false) {
     Write-Error ""
 	Write-Error "#teamcity[buildStatus status='FAILURE' text='Failed to install $packageIdToInstall']"
 }
-
 
 [string[]] $repoList = Git-LoadRepoList -repoFile $repos
 
