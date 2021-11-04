57545cbde32446fac633e553612fa391cbbc4ea2
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-11-04 20:23:08 +0000

    Detect build version

---
+++ powershell/lib/BuildVersion.psm1
@@ -0,0 +1,24 @@
+﻿<#
+ .Synopsis
+  Gets the next patch release version
+
+ .Description
+  Gets the next patch release version
+#>
+function BuildVersion-GetNextPatch {
+    $result = dotnet buildversion --BuildNumber 9999 --WarningsAsErrors
+
+    if(!$?) {
+        return $null
+    }
+
+    $match = select-string "Version:\s(\d+\.\d+\.\d+)\.\d+\-master" -InputObject $result
+    if($match) {
+        $version = $match.Matches.Groups[1].Value
+        return $version
+    }
+
+    return $null
+}
+
+Export-ModuleMember -Function BuildVersion-GetNextPatch
