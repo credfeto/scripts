28695138fc53bcbde282f5ac1caa88e9995af32b
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-11-09 12:56:34 +0000

    Removed stupid echo

--- general/dependencies.cmd
+++ general/dependencies.cmd
@@ -1,6 +1,6 @@
 @echo off
 cls 
-echo Echo processing projects in %CD%...
+echo processing projects in %CD%...
 git reset head --hard
 dotnet restore
 powershell -file d:\Work\Personal\scripts\powershell\RemoveRedundantReferences.ps1 -solutionDirectory %CD%
