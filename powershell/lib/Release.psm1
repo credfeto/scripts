d5eb500bc9cba84752b7da7b0ffa11497d96be9f
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-12-05 18:23:10 +0000

    Extracted Release-Create from MakeRelease

---
+++ powershell/lib/Release.psm1
@@ -0,0 +1,27 @@
+﻿function Release-Create {
+param(
+    [string]$repo, 
+    [string]$changeLog, 
+    [string]$repoPath
+)
+     $nextPatch = BuildVersion-GetNextPatch
+     if($nextPatch) {
+#         ChangeLog-CreateRelease -fileName $changeLog -release $nextPatch
+#         Git-Commit -message "Release notes for $nextPatch"
+#         Git-Push --repoPath  $repoPath
+# 
+         $branch = "release/$nextPatch"
+         Write-Information "MAKERELEASE: Should have created branch: $branch"
+
+#         $branched = Git-CreateBranch -branchName $branch -repoPath $repoPath
+#         if($branch) {
+#             Git-PushOrigin -branchName $branch -repoPath $repoPath
+#             Write-Information "*** Created new release branch $branch in $repo"
+#         }
+     }
+}
+
+
+
+
+Export-ModuleMember -Function Release-Create
