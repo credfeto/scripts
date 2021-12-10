010e1284c3be8f619d19f3ae07c13a14a55ad025
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-12-10 15:51:48 +0000

    Enabled creating releases on updates

--- powershell/lib/Release.psm1
+++ powershell/lib/Release.psm1
@@ -8,20 +8,20 @@ param(
     [string]$nextPatch = BuildVersion-GetNextPatch
     if($nextPatch) {
         Write-Information "$repo => Create release $nextPatch"
-#         ChangeLog-CreateRelease -fileName $changeLog -release $nextPatch
-#         Git-Commit -message "Release notes for $nextPatch"
-#         Git-Push --repoPath  $repoPath
-# 
+         ChangeLog-CreateRelease -fileName $changeLog -release $nextPatch
+         Git-Commit -message "Release notes for $nextPatch"
+         Git-Push --repoPath  $repoPath
+ 
          [string]$branch = "release/$nextPatch"
          Write-Information "RELEASE: Should have created branch: $branch"
 
-#         $branched = Git-CreateBranch -branchName $branch -repoPath $repoPath
-#         if($branch) {
-#             Git-PushOrigin -branchName $branch -repoPath $repoPath
-#             Write-Information "*** Created new RELEASE branch $branch in $repo"
-#         } else {
-#             throw "Failed to create RELEASE branch $branch in $repo"
-#         }
+         $branched = Git-CreateBranch -branchName $branch -repoPath $repoPath
+         if($branch) {
+             Git-PushOrigin -branchName $branch -repoPath $repoPath
+             Write-Information "*** Created new RELEASE branch $branch in $repo"
+         } else {
+             throw "Failed to create RELEASE branch $branch in $repo"
+         }
     } else {
         throw "No RELEASE version found for $repo"
     }
