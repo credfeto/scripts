024e5bdc79d815006af604588c0ecefdec762055
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-02-06 22:31:33 +0000

    Corrected typo

--- powershell/UpdateFromTemplate.ps1
+++ powershell/UpdateFromTemplate.ps1
@@ -448,7 +448,7 @@ param(
     Write-Information "Config Path: $srcPath"
     [string]$targetFileName = makePath -Path $targetRepo -ChildPath ".github/dependabot.yml"
 
-    [bool]$updateGitHubActions = $hasNonTemplateWorkFlows -And !$targetRepo.ToLowerInvariant().Contains("fFunfair")
+    [bool]$updateGitHubActions = $hasNonTemplateWorkFlows -And !$targetRepo.ToLowerInvariant().Contains("funfair")
 
     [bool]$hasSubModules = Git-HasSubModules -repoPath $targetRepo 
     Dependabot-BuildConfig -configFileName $targetFileName -repoRoot $targetRepo -updateGitHubActions $updateGitHubActions -hasSubModules $hasSubModules
