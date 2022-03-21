508ed791ab1ea23f3d78efff17d7294a83145503
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-03-21 22:38:57 +0000

    Generating appsettings.json

---
+++ powershell/DBAppSettings.ps1
@@ -0,0 +1,40 @@
+param(
+    [string] $server = $(throw "Server"),
+    [string] $user = $(throw "User"),
+    [string] $password = $(throw "Password"),
+    [string] $database = $(throw "Database")
+)
+
+Remove-Module *
+Set-StrictMode -Version 1
+
+$InformationPreference = "Continue"
+$ErrorActionPreference = "Stop"
+
+$files = Get-ChildItem -Path . -Filter appsettings.json -Recurse
+ForEach($file in $files) {
+    $readFile = $file.FullName
+    $sourceProperties = Get-Content -Raw -Path $readFile | ConvertFrom-Json
+    
+    if($sourceProperties.DatabaseConfiguration.ConnectionString) {
+        $fileToUpdate = $readFile.Replace(".json", "-local.json")
+        
+        $updateFileExists = Test-Path -Path $fileToUpdate
+        if($updateFileExists) {
+            Write-Information "* Updating $fileToUpdate"
+            $properties = Get-Content -Raw -Path $readFile | ConvertFrom-Json
+        }
+        else {
+            Write-Information "* Creating $fileToUpdate"
+            $properties = ConvertFrom-Json '{"DatabaseConfiguration": {"Provider":"","ConnectionString":""}}'
+        }
+
+        Write-Information "  - Setting DatabaseConfiguration:Provider"
+        $properties.DatabaseConfiguration.Provider = "mssql"
+        
+        Write-Information "  - Setting DatabaseConfiguration:ConnectionString"
+        $properties.DatabaseConfiguration.ConnectionString = "Database=$database;Server=$server;User ID=$user;Password=$password;Application Name=$database;Connection Timeout=60"
+
+        $properties | ConvertTo-Json -Compress | Out-File -encoding ASCII $fileToUpdate
+    }
+}
