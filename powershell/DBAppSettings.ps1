6241623ee101557d965e8d04cffa2c1563d237bd
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-03-21 22:43:52 +0000

    Only create/update appsettings-local.json in folders that have a .csproj

--- powershell/DBAppSettings.ps1
+++ powershell/DBAppSettings.ps1
@@ -14,27 +14,36 @@ $ErrorActionPreference = "Stop"
 $files = Get-ChildItem -Path . -Filter appsettings.json -Recurse
 ForEach($file in $files) {
     $readFile = $file.FullName
+    $folder = $file.Directory.FullName
+    $projects = Get-ChildItem -Path $folder -Filter *.csproj
+    if( !$projects) {
+        Continue
+    }
+        
     $sourceProperties = Get-Content -Raw -Path $readFile | ConvertFrom-Json
     
-    if($sourceProperties.DatabaseConfiguration.ConnectionString) {
-        $fileToUpdate = $readFile.Replace(".json", "-local.json")
-        
-        $updateFileExists = Test-Path -Path $fileToUpdate
-        if($updateFileExists) {
-            Write-Information "* Updating $fileToUpdate"
-            $properties = Get-Content -Raw -Path $readFile | ConvertFrom-Json
-        }
-        else {
-            Write-Information "* Creating $fileToUpdate"
-            $properties = ConvertFrom-Json '{"DatabaseConfiguration": {"Provider":"","ConnectionString":""}}'
-        }
-
-        Write-Information "  - Setting DatabaseConfiguration:Provider"
-        $properties.DatabaseConfiguration.Provider = "mssql"
-        
-        Write-Information "  - Setting DatabaseConfiguration:ConnectionString"
-        $properties.DatabaseConfiguration.ConnectionString = "Database=$database;Server=$server;User ID=$user;Password=$password;Application Name=$database;Connection Timeout=60"
-
-        $properties | ConvertTo-Json -Compress | Out-File -encoding ASCII $fileToUpdate
+    if( !$sourceProperties.DatabaseConfiguration.ConnectionString) {
+        continue
     }
+    
+    $fileToUpdate = $readFile.Replace(".json", "-local.json")
+    
+    $updateFileExists = Test-Path -Path $fileToUpdate
+    if($updateFileExists) {
+        Write-Information "* Updating $fileToUpdate"
+        $properties = Get-Content -Raw -Path $readFile | ConvertFrom-Json
+    }
+    else {
+        Write-Information "* Creating $fileToUpdate"
+        $properties = ConvertFrom-Json '{"DatabaseConfiguration": {"Provider":"","ConnectionString":""}}'
+    }
+
+    Write-Information "  - Setting DatabaseConfiguration:Provider"
+    $properties.DatabaseConfiguration.Provider = "mssql"
+    
+    Write-Information "  - Setting DatabaseConfiguration:ConnectionString"
+    $properties.DatabaseConfiguration.ConnectionString = "Database=$database;Server=$server;User ID=$user;Password=$password;Application Name=$database;Connection Timeout=60"
+
+    $properties | ConvertTo-Json -Compress | Out-File -encoding ASCII $fileToUpdate
+
 }
