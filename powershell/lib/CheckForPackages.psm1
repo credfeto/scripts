baba758af5e8b778967144cb9da878d29a38bb92
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-12-21 12:49:18 +0000

    Moving package update checks to lib

--- powershell/lib/CheckForPackages.psm1
+++ powershell/lib/CheckForPackages.psm1
@@ -31,5 +31,162 @@ param (
     return $false
 }
 
+
+function buildPackageSearch{
+    param(
+        [string]$packageId,
+        [bool]$exactMatch
+    )
+    
+    if($exactMatch) {
+        return $packageId
+    }
+    
+    return "$($packageId):prefix"
+}
+
+function buildExcludes{
+param(
+    $exclude
+    )
+    
+    $excludes =@()
+    foreach($item in $exclude)
+    {
+        [string]$packageId = $item.packageId
+        [boolean]$exactMatch = $item.'exact-match'
+        $search = buildPackageSearch -packageId $packageId -exactMatch $exactMatch         
+        $excludes += $search
+    }
+    
+    if($excludes.Count -gt 0) {
+        $excluded = $excludes -join " "
+        Write-Information "Excluding: $excluded"
+       
+        return $excludes
+    }
+    else {
+        Write-Information "Excluding: <<None>>"
+        return $null        
+    }
+}
+
+
+function Packages_CheckForUpdatesExact{
+param(
+    [String]$repoFolder = $(throw "Packages_CheckForUpdatesExact: repoFolder not specified"), 
+    [String]$packageId = $(throw "Packages_CheckForUpdatesExact: packageId not specified"),
+    $exclude    
+    )
+
+    $restore = dotnet tool restore
+    if (!$?) {
+       throw $restore
+    }
+
+    Write-Information "Updating Package Exact"
+    $search = buildPackageSearch -packageId $packageId -exactMatch $True
+    $excludes = buildExcludes -exclude $exclude
+    if($excludes) {
+        $results = dotnet updatepackages --cache $packageCache --folder $repoFolder --package-id $search --exclude $excludes
+    }
+    else {
+        $results = dotnet updatepackages --cache $packageCache --folder $repoFolder --package-id $search
+    }    
+    if ($?)
+    {
+        WriteLogs -logs $results
+        
+        # has updates
+        [string]$packageIdAsRegex = $packageId.Replace(".", "\.").ToLower()
+        [string]$regexPattern = "::set-env name=$packageIdAsRegex::(?<Version>\d+(\.\d+)+)"
+
+        $regex = new-object System.Text.RegularExpressions.Regex($regexPattern, [System.Text.RegularExpressions.RegexOptions]::MultiLine)
+        $regexMatches = $regex.Matches($results.ToLower());
+        if($regexMatches.Count -gt 0) {
+            [string]$version = $regexMatches[0].Groups["Version"].Value
+            Write-Information "Found: $version"
+            return $version
+        }
+        
+        Write-Information " * No Changes"    
+    }
+    else
+    {
+        Write-Information " * ERROR: Failed to update $packageId"    
+        WriteLogs -logs $results
+    }
+
+    
+    return $null
+}
+
+
+function Packages_CheckForUpdatesPrefix{
+param(
+    [String]$repoFolder = $(throw "checkForUpdatesPrefix: repoFolder not specified"),
+    [String]$packageId = $(throw "checkForUpdatesPrefix: packageId not specified"),
+    $exclude
+    )
+
+    $restore = dotnet tool restore
+    if (!$?) {
+       throw $restore
+    }
+
+    Write-Information "Updating Package Prefix"
+    $search = buildPackageSearch -packageId $packageId -exactMatch $False
+    $excludes = buildExcludes -exclude $exclude
+    if($excludes) {
+        $results = dotnet updatepackages --cache $packageCache --folder $repoFolder --package-id $search --exclude $excludes
+    }
+    else {
+        $results = dotnet updatepackages --cache $packageCache --folder $repoFolder --package-id $search
+    }
+
+    if($?) {
+        
+        WriteLogs -logs $results
+        
+        # has updates
+        [string]$packageIdAsRegex = $packageId.Replace(".", "\.").ToLower()
+        [string]$regexPattern = "echo ::set-env name=$packageIdAsRegex(.*?)::(?<Version>\d+(\.\d+)+)"
+
+        $regex = new-object System.Text.RegularExpressions.Regex($regexPattern, [System.Text.RegularExpressions.RegexOptions]::MultiLine)
+        $regexMatches = $regex.Matches($results.ToLower());
+        if($regexMatches.Count -gt 0) {
+            [string]$version = $regexMatches[0].Groups["Version"].Value
+            Write-Information "Found: $version"
+            return $version
+        }
+    }
+    else
+    {
+        WriteLogs -logs $results
+    }
+    
+    Write-Information " * No Changes"    
+    return $null
+}
+
+function Packages_CheckForUpdates{
+param(
+    [String]$repoFolder = $(throw "Packages_CheckForUpdates: repoFolder not specified"),
+    [String]$packageId = $(throw "Packages_CheckForUpdates: packageId not specified"),
+    [Boolean]$exactMatch,
+    $exclude
+)
+
+    if ($exactMatch -eq $true)
+    {
+        return Packages_CheckForUpdatesExact -repoFolder $repoFolder -packageId $packageId -exclude $exclude
+    }
+    else
+    {
+        return Packages_CheckForUpdatesPrefix -repoFolder $repoFolder -packageId $packageId -exclude $exclude
+    }
+}
+
 Export-ModuleMember -Function Packages_Get
 Export-ModuleMember -Function Packages_ShouldUpdate
+Export-ModuleMember -Function Packages_CheckForUpdates
