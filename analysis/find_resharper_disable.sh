c486cde855b098ffeff4110263a58a4051eaddd1
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-05-05 22:05:05 +0100

    Use /bin/sh

--- analysis/find_resharper_disable.sh
+++ analysis/find_resharper_disable.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
+#! /bin/sh
 
 find . -type f -iname '*.cs' -print0 | xargs -I {} -0 grep --no-filename '// ReSharper disable once ' "{}" | sed -e 's/^\s*//' -e '/^$/d' | sort -b | uniq > suppressions.txt
