4b88cd2199067bc3a5f248183fc8a995992f0fe9
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-11-14 11:26:40 +0000

    npm.local

--- docker/baget/trust.sh
+++ docker/baget/trust.sh
@@ -3,3 +3,4 @@
 sudo trust anchor --store ./certs/api.nuget.local.pem
 sudo trust anchor --store ./certs/funfair.nuget.local.pem
 sudo trust anchor --store ./certs/funfair-prerelease.nuget.local.pem
+sudo trust anchor --store ./certs/npm.local.pem
